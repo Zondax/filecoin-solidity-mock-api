4997f2eb20bd8417410b415cdbc2728861d3f00f
Emmanuel <emmanuelm41@gmail.com>
2022-11-18 10:51:46 -0300

    make market test contract useful to test market api

--- contracts/v0.8/testContracts/market.test.sol
+++ contracts/v0.8/testContracts/market.test.sol
@@ -12,13 +12,14 @@ contract FilecoinMarketMockTest {
         marketApiAddress = _marketApiAddress;
     }
 
-    function market_withdraw_balance_test() public view{
+    function market_withdraw_balance_test() public view returns (MarketTypes.WithdrawBalanceReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
         string memory addr = "t01113";
         MarketTypes.WithdrawBalanceParams memory params = MarketTypes.WithdrawBalanceParams(addr, 1);
 
         MarketTypes.WithdrawBalanceReturn memory response = marketApiInstance.withdraw_balance(params);
+        return response;
     }
 
     function market_add_balance_test() public{
@@ -30,92 +31,102 @@ contract FilecoinMarketMockTest {
         marketApiInstance.add_balance(params);
     }
 
-    function get_balance_test() public view {
+    function get_balance_test() public view returns (MarketTypes.GetBalanceReturn memory) {
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
         string memory params = "t01113";
 
         MarketTypes.GetBalanceReturn memory response = marketApiInstance.get_balance(params);
+        return response;
     }
 
-    function get_deal_data_commitment_test() public view{
+    function get_deal_data_commitment_test() public view returns (MarketTypes.GetDealDataCommitmentReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealDataCommitmentParams memory params = MarketTypes.GetDealDataCommitmentParams(1);
+        MarketTypes.GetDealDataCommitmentParams memory params = MarketTypes.GetDealDataCommitmentParams(75);
 
         MarketTypes.GetDealDataCommitmentReturn memory response = marketApiInstance.get_deal_data_commitment(params);
+        return response;
     }
 
-    function get_deal_client_test() public view{
+    function get_deal_client_test() public view returns (string memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealClientParams memory params = MarketTypes.GetDealClientParams(1);
+        MarketTypes.GetDealClientParams memory params = MarketTypes.GetDealClientParams(74);
 
         MarketTypes.GetDealClientReturn memory response = marketApiInstance.get_deal_client(params);
+        return response.client;
     }
 
-    function get_deal_provider_test() public view{
+    function get_deal_provider_test() public view returns (MarketTypes.GetDealProviderReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealProviderParams memory params = MarketTypes.GetDealProviderParams(1);
+        MarketTypes.GetDealProviderParams memory params = MarketTypes.GetDealProviderParams(80);
 
         MarketTypes.GetDealProviderReturn memory response = marketApiInstance.get_deal_provider(params);
+        return response;
     }
 
-    function get_deal_label_test() public view{
+    function get_deal_label_test() public view returns (MarketTypes.GetDealLabelReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealLabelParams memory params = MarketTypes.GetDealLabelParams(1);
+        MarketTypes.GetDealLabelParams memory params = MarketTypes.GetDealLabelParams(73);
 
         MarketTypes.GetDealLabelReturn memory response = marketApiInstance.get_deal_label(params);
+        return response;
     }
 
-    function get_deal_term_test() public view{
+    function get_deal_term_test() public view returns (MarketTypes.GetDealTermReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealTermParams memory params = MarketTypes.GetDealTermParams(1);
+        MarketTypes.GetDealTermParams memory params = MarketTypes.GetDealTermParams(72);
 
         MarketTypes.GetDealTermReturn memory response = marketApiInstance.get_deal_term(params);
+        return response;
     }
 
-    function get_deal_epoch_price_test() public view{
+    function get_deal_epoch_price_test() public view returns (MarketTypes.GetDealEpochPriceReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealEpochPriceParams memory params = MarketTypes.GetDealEpochPriceParams(1);
+        MarketTypes.GetDealEpochPriceParams memory params = MarketTypes.GetDealEpochPriceParams(67);
 
         MarketTypes.GetDealEpochPriceReturn memory response = marketApiInstance.get_deal_epoch_price(params);
+        return response;
     }
 
-    function get_deal_client_collateral_test() public view{
+    function get_deal_client_collateral_test() public view returns (MarketTypes.GetDealClientCollateralReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealClientCollateralParams memory params = MarketTypes.GetDealClientCollateralParams(1);
+        MarketTypes.GetDealClientCollateralParams memory params = MarketTypes.GetDealClientCollateralParams(68);
 
         MarketTypes.GetDealClientCollateralReturn memory response = marketApiInstance.get_deal_client_collateral(params);
+        return response;
     }
 
-    function get_deal_provider_collateral_test() public view{
+    function get_deal_provider_collateral_test() public view returns (MarketTypes.GetDealProviderCollateralReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealProviderCollateralParams memory params = MarketTypes.GetDealProviderCollateralParams(1);
+        MarketTypes.GetDealProviderCollateralParams memory params = MarketTypes.GetDealProviderCollateralParams(69);
 
         MarketTypes.GetDealProviderCollateralReturn memory response = marketApiInstance.get_deal_provider_collateral(params);
+        return response;
     }
 
-    function get_deal_verified_test() public view{
+    function get_deal_verified_test() public view returns (MarketTypes.GetDealVerifiedReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        MarketTypes.GetDealVerifiedParams memory params = MarketTypes.GetDealVerifiedParams(1);
+        MarketTypes.GetDealVerifiedParams memory params = MarketTypes.GetDealVerifiedParams(70);
 
         MarketTypes.GetDealVerifiedReturn memory response = marketApiInstance.get_deal_verified(params);
+        return response;
     }
 
-    function get_deal_activation_test() public view{
+    function get_deal_activation_test() public view returns (MarketTypes.GetDealActivationReturn memory){
         MarketAPI marketApiInstance = MarketAPI(marketApiAddress);
 
-        uint64 id;
-        MarketTypes.GetDealActivationParams memory params = MarketTypes.GetDealActivationParams(id);
+        MarketTypes.GetDealActivationParams memory params = MarketTypes.GetDealActivationParams(71);
 
         MarketTypes.GetDealActivationReturn memory response = marketApiInstance.get_deal_activation(params);
+        return response;
     }
 }
